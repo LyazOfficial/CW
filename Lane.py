5044e67f6e516e43fcadcf523b49e19007cc906c
LyazOfficial <153516881+LyazOfficial@users.noreply.github.com>
2023-12-11 13:26:37 +0000

    Update Lane.py

--- Lane.py
+++ Lane.py
@@ -1,10 +1,35 @@
 import time
 import datetime
+import Customer
+
+
 class Lane:
-    def __init__(self,lane_type,status):
+    def __init__(self, lane_type):
         self.lane_type = lane_type
-        self.status = status
+        self.status = "Closed"
         self.timestamp = datetime.datetime.now()
         self.customer = []
-    def add_customer_to_lane(self, custmoer):
-        self.customer.append(custmoer)
+
+    def add_customer_to_lane(self, customer):
+        self.customer.append(customer)
+
+
+class RegularLane(Lane):
+    def __init__(self):
+        super().__init__("Regular")
+        self.till = 1
+
+
+class SelfServiceLane(Lane):
+    def __init__(self):
+        super().__init__("SelfService")
+        self.till = 8
+
+
+Lane1 = RegularLane()
+print(Lane1)
+
+Lane2 = SelfServiceLane()
+print(Lane2)
+
+
